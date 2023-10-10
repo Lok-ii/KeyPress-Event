05c5f65ca6ddf628be42679172d3abb9dcbcdcb6
Lok-ii <lokeshkataria.work@gmail.com>
2023-10-10 17:30:34 +0530

    completed

---
+++ script.js
@@ -0,0 +1,28 @@
+let keyName = document.querySelector(".keyName");
+let keyCode = document.querySelector(".keyCode");
+let text = document.querySelector(".text");
+
+let list = document.querySelector(".keyHistory");
+let audio = new Audio("./keyPress.mp3");
+document.addEventListener("keydown", (e) =>{
+    audio.play();
+    let name = e.key;
+    if(e.key === " "){
+        keyName.innerText = "SpaceBar";
+        name = "SpaceBar";
+    }else{
+        keyName.innerText = name;
+        
+    }
+    text.innerText = "You Pressed "
+    keyCode.innerText = e.keyCode;
+
+    let li = document.createElement("li");
+    li.innerText = `${name} : ${e.keyCode}`;
+    insertAfter(li, list.firstElementChild);
+    // list.appendChild(li);
+});
+
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
